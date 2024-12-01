6b8a1548fb2adc885c71e3439a8fee0194ced293
Kayque Pereira <114672278+jKayquePereira@users.noreply.github.com>
2024-12-01 18:04:48 -0300

    pequena limpeza do codigo

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const containerEl = document.querySelector(".container")
-function createGrid(num) {
-    const numDefault = 16
+function createGrid(size) {
 
-    if (num > 100 || num < 2) {
-        num = numDefault
+    if (size > 100 || size < 2) {
+        size = 16
     }
 
-    for (i = 0; i < num * num; i++) {
+    for (let i = 0; i < size * size; i++) {
         const grid = document.createElement("div")
         grid.classList.add("grid")
         containerEl.appendChild(grid)
