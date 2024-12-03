15daf7c60a7bd86d63389953d2c9743c33b8835d
Kayque Pereira <114672278+jKayquePereira@users.noreply.github.com>
2024-12-02 22:39:32 -0300

    correcao do problema da grade que fugia do espaco do container, ao inves de se adaptar a ele.

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const containerEl = document.querySelector(".container")
+
 function createGrid(size) {
+    const grid = document.createElement("div")
+    grid.classList.add("grid")
+    containerEl.appendChild(grid)
+    grid.innerHTML = ''
+
+    const cellSize = 100 / size
 
     if (size > 100 || size < 2) {
         size = 16
     }
 
     for (let i = 0; i < size * size; i++) {
-        const grid = document.createElement("div")
-        grid.classList.add("grid")
-        containerEl.appendChild(grid)
+        const cell = document.createElement("div")
+
+        cell.style.flex = `0 0 ${cellSize}%`
+        cell.style.height = `${cellSize}%`
+        cell.classList.add("cell")
+        grid.appendChild(cell)
     }
 }
 
 createGrid(16)
+
